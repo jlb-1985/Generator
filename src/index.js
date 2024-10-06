68422c9a683c3dc191bf867ce32f12052be161e3
jlb-1985 <130216366+jlb-1985@users.noreply.github.com>
2024-10-06 15:36:45 +0200

    Correct a typo in HTML and add API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function displayFacts(data) {
+        new Typewriter("#result", {
+            strings: data.map(fact => fact.fact),
+            autoStart: true,
+            delay: 1,
+            cursor: "",
+        });
+    }
 
-function generateFacts(event) {
-    event.preventDefault();
+    async function generateFacts(event) {
+        event.preventDefault();
 
+        const axios = await import('axios');
 
-new Typewriter("#result", {
-    strings: "facts facts",
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
+        const options = {
+            method: 'GET',
+            url: 'https://factopia.p.rapidapi.com/fact',
+            headers: {
+                'x-rapidapi-key': 'd3bade6b45msh554bcd1b7ef7561p1ad2e1jsnc22cc80bc110',
+                'x-rapidapi-host': 'factopia.p.rapidapi.com'
+            }
+        };
+        
+        try {
+            const response = await axios.request(options);
+            console.log(response.data);
+            displayFacts(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-}
-
-let factsFormElement = document.querySelector("#facts-generator-form");
-factsFormElement.addEventListener("submit", generateFacts);
+    let factsFormElement = document.querySelector("#facts-generator-form");
+    factsFormElement.addEventListener("submit", generateFacts);
+});
