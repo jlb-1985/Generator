bff15e365df65bb7bfa4a3afd0d9f82c0f4e64da
jlb-1985 <130216366+jlb-1985@users.noreply.github.com>
2024-10-06 18:58:55 +0200

    Updated CSS and update context

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function showFacts(event) {
   let instructionsInput = document.querySelector("#user-instructions").value;
   let apiKey = "135f02bffcta720ff4c241394b466o99"; // Ensure this is your actual API key
   let prompt = `User instructions: Generate a fact about ${instructionsInput}`;
-  let context = "You are a factchecker expert and love to research facts and educate others about facts. Your mission is to generate a fact and describe it in maximum 4 lines. Make sure you follow the user instructions. Begin and end with a emoticon";
+  let context = "You are a factchecker expert and love to research facts and educate others about facts. Your mission is to generate a fact and describe it in maximum 4 lines. Make sure you follow the user instructions. Begin and end with a matching emoticon";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating facts");
